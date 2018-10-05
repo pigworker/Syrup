140a0a94eee585e2c232bbb04deae4f7a162512b
Conor McBride <conor@strictlypositive.org>
2018-10-05 09:11:43 +0100

    top-level syrup

--- SRC/Main.hs
+++ SRC/Main.hs
@@ -7,6 +7,7 @@
 module Syrup.SRC.Main where
 
 import Data.List
+import Control.Arrow
 
 import Syrup.SRC.Syn
 import Syrup.SRC.Par
@@ -14,11 +15,11 @@ import Syrup.SRC.Ty
 import Syrup.SRC.Chk
 import Syrup.SRC.Expt
 
-grok :: CoEnv -> [Either [String] (Source, String)] -> [String]
-grok env [] = []
-grok env (Left ss : src) = ss ++ "" : grok env src
+grok :: CoEnv -> [Either [String] (Source, String)] -> (CoEnv, [String])
+grok env [] = (env, [])
+grok env (Left ss : src) = (id *** ((ss ++) . ("" :))) (grok env src)
 grok env (Right (Declaration dec@(DEC (f, _) _), s) : src) =
-  drept ++ trept ++ [""] ++ grok env' src where
+  (id *** ((drept ++) . (trept ++) . ("" :))) (grok env' src) where
     (_, trept, env') = mkComponent env (dec, s) mdef
     (drept, mdef) = case
         [ (def, s)
@@ -33,10 +34,13 @@ grok env (Right (Declaration dec@(DEC (f, _) _), s) : src) =
         , Nothing)
       [] -> (["You haven't defined " ++ f ++ " just now.", ""], Nothing)
 grok env (Right (Experiment expt, _) : src) =
-  experiment env expt ++ "" : grok env src
+  (id *** ((experiment env expt ++) . ("" :))) (grok env src)
 grok env (Right (Definition _, _) : src) = grok env src
+
+syrup :: CoEnv -> String -> (CoEnv, String)
+syrup g = (id *** unlines) . grok g . syrupFile
 
 main :: IO ()
 main = do
-  src <- syrupFile <$> getContents
-  foldMap putStrLn (grok myCoEnv src)
+  src <- getContents
+  putStr (snd (syrup myCoEnv src))
