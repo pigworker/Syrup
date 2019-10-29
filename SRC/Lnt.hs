06e56422393bb141f17ef121fe8b31d3ba06169a
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-10-29 09:55:13 +0000

    [ refactor ] make it easier to have multiple analyses

--- SRC/Lnt.hs
+++ SRC/Lnt.hs
@@ -4,33 +4,48 @@
 -----                                                                    -----
 ------------------------------------------------------------------------------
 
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE GADTs             #-}
 
 module Syrup.SRC.Lnt where
 
 import Syrup.SRC.Syn
 
 class Lint t where
-  lint :: t -> Maybe [String]
-  lint _ = Nothing
+  linters :: [t -> [[String]]]
+  linters = []
+
+lint :: Lint t => t -> [[String]]
+lint t = foldMap ($ t) linters
 
 instance Lint Def where
-  lint = \case
-    Def (fun, _) _ (Just []) -> Just $
-      [ "Warning: empty where clause in the definition of " ++ fun ++ "."
-      , "Did you forget to indent the block of local definitions using spaces?"
-      ]
-    _ -> Nothing
+  linters = [ emptyWhere
+            ] where
+
+    emptyWhere = \case
+      Def (fun, _) _ (Just []) -> pure $
+        [ "Warning: empty where clause in the definition of " ++ fun ++ "."
+        , "Did you forget to indent the block of local definitions using spaces?"
+        ]
+      _ -> []
+
+    uselessMatch = \case
+      Def (fun, ps) rhs eqns -> undefined
+      _ -> []
+
 
 instance Lint (Source' a) where
-  lint = \case
-    Definition d -> lint d
-    _ -> Nothing
+  linters = [deflint] where
+
+    deflint = \case
+      Definition d -> lint d
+      _ -> []
 
 linter :: Lint t
        => [Either [String] (t, String)]
        -> [Either [String] (t, String)]
 linter xs = xs >>= \case
   err@Left{}         -> [err]
-  src@(Right (t, _)) -> maybe id ((:) . Left) (lint t) [src]
+  src@(Right (t, _)) -> map Left (lint t) ++ [src]
 
