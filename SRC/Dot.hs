34409ff77a1f08b3f8df0cc0b4e58dd291bad687
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-12-08 15:03:17 +0000

    [ dot ] whitebox gate

--- SRC/Dot.hs
+++ SRC/Dot.hs
@@ -15,6 +15,7 @@
 
 module Syrup.SRC.Dot where
 
+import Control.Monad.Writer
 import Control.Monad.State
 import Data.List
 import Data.Maybe
@@ -24,9 +25,13 @@ import Syrup.SRC.Syn
 import Syrup.SRC.Anf
   ( Input'(..), Input
   , Output'(..), Output
+  , Expr'(..)
   , Gate(..)
   , toGate
+  , evalFresh
+  , Fresh
   )
+import qualified Syrup.SRC.Anf as Anf
 
 data DotGate = DotGate
   { blackbox    :: [String]
@@ -48,7 +53,7 @@ extend :: Int -> Path -> Path
 extend i (Path is) = Path (i : is)
 
 instance Show Path where
-  show (Path ps) = intercalate "-" (show <$> reverse ps)
+  show (Path ps) = intercalate "__" (show <$> reverse ps)
 
 data DotSt = DotSt
   { supply :: Int
@@ -67,8 +72,8 @@ newtype Dot a = Dot { runDot :: StateT DotSt Maybe a }
 evalDot :: Dot a -> Maybe a
 evalDot = flip evalStateT initDotSt . runDot
 
-fresh :: Dot Int
-fresh = Dot $ do
+freshId :: Dot Int
+freshId = Dot $ do
   s <- get
   let i = supply s
   put $ s { supply = i + 1 }
@@ -100,66 +105,167 @@ connectOutputs nm ns = unlines $ indent 2 <$>
   ]
 
 mkNode :: Path -> String -> String
-mkNode p str = "NODE_" ++ str ++ "_" ++ show p
+mkNode p str = show $ "NODE_" ++ str ++ "_" ++ show p
 
 mkGate :: Path -> String -> String
-mkGate p str = "GATE_" ++ str ++ "_" ++ show p
+mkGate p str = show $ "GATE_" ++ str ++ "_" ++ show p
+
+mkArrow :: Path -> String -> String -> String
+mkArrow p x y = arrow True (mkNode p x) (mkNode p y)
+
+arrow :: Bool -> String -> String -> String
+arrow b x y = concat
+  [ x
+  , " -> "
+  , y
+  , if b then ";" else " [dir = none];"
+  ]
 
 declareInput :: Path -> Input -> String
 declareInput p Input{..} =
-  mkNode p inputName ++ " [label = " ++ inputName ++ "];"
+  mkNode p inputName ++ " [label = " ++ show inputName ++ "];"
 
 declareOutput :: Path -> Output -> String
 declareOutput p Output{..} =
   let name = mkNode p outputName in
-  if isVirtual then name ++ ";" else (name ++ " [label = " ++ outputName ++ "];")
+  name ++ " [label = " ++ (if isVirtual then show "" else show outputName) ++ "];"
+
+declareLocal :: Path -> Output -> String
+declareLocal p Output{..} =
+  let name = mkNode p outputName in
+  name ++ " [shape = point, height = 0];"
+
+
+-- The monad we use to generate the content of the whitebox implementation:
+-- we need a name supply & we would rather use Writer to collect the lines
+-- of code rather than having to assemble everything by hand.
+newtype WhiteBox a = WhiteBox { runWhiteBox :: WriterT [String] Fresh a }
+  deriving (Functor, Applicative, Monad, MonadWriter [String])
+
+execWhiteBox :: WhiteBox () -> [String]
+execWhiteBox = evalFresh . execWriterT . runWhiteBox
+
+fresh :: WhiteBox Int
+fresh = WhiteBox $ lift Anf.fresh
+
+nodeCluster :: Path -> String -> [String] -> [String]
+nodeCluster p name nodes =
+  [ "  subgraph cluster_" ++ name ++ "_" ++ show p ++ " {"
+  , "    style = invis;"
+  , "    node [shape = none];"
+  ,      unlines $ map (indent 4) nodes
+  , "  }"
+  ]
 
 gate :: String -> Gate -> Arr String (Path -> DotGate)
      -> Path -> DotGate
 gate nm Gate{..} env p =
-  let gateNode    = mkGate p nm
-      inputNodes  = map (mkNode p . inputName)  inputs
-      outputNodes = map (mkNode p . outputName) outputs
+  DotGate { inputNodes  = theInputs
+          , outputNodes = theOutputs
+          , blackbox    = theBlackbox
+          , whitebox    = theWhitebox
+          } where
 
-      blackbox    = filter (/= "")
-        [ "subgraph cluster_" ++ show p ++ " {"
-        , "  style = invis;"
-        , indent 2 $ gateNode ++ " [label = " ++ show nm ++ "];"
-        , "  {"
-        , "    node [shape = point, height = 0];"
-        ,      unlines (map (indent 4 . declareInput p) inputs)
-            ++ unlines (map (indent 4 . declareOutput p) outputs)
-        , "  }"
-        , leftToRight "inputs" inputNodes
-        , leftToRight "outputs" outputNodes
-        , connectInputs inputNodes gateNode
-        , connectOutputs gateNode outputNodes
-        , "}"
-        ]
+  gateNode   = mkGate p nm
+  theInputs  = map (mkNode p . inputName)  inputs
+  theOutputs = map (mkNode p . outputName) outputs
 
-      whitebox = []
-  in DotGate { blackbox, whitebox, inputNodes, outputNodes }
+  header str  = unlines $
+    [ ""
+    , "/***************************************************/"
+    ] ++ str ++
+    [ "/***************************************************/" ]
+
+  theWhitebox = execWhiteBox $ do
+    tell [ header [ "// The circuit's inputs and outputs" ] ]
+    tell $ nodeCluster p "inputs" $ map (declareInput p) inputs
+    tell $ nodeCluster p "outputs" $ map (declareOutput p) outputs
+
+    tell [ "  node [shape = rectangle];" ]
+
+    forM_ (reverse definitions) $ \ (os, e) -> do
+      forM_ os $ \ o -> tell [ declareLocal p o ]
+      case e of
+        Alias x     -> case os of
+          [y] -> tell [indent 2 $ mkArrow p x (outputName y) ]
+          _   -> error "not yet supported"
+        Call f args -> case findArr f env of
+          Nothing   -> error "This should never happen"
+          Just repr -> do
+            id <- fresh
+            let dotG = repr (extend id p)
+            tell (blackbox dotG)
+            forM_ (zip args (inputNodes dotG)) $ \ (arg, iport) ->
+              tell [ arrow False (mkNode p (inputName arg)) iport ]
+            forM_ (zip (outputNodes dotG) os) $ \ (oport, out) -> do
+              let outName = mkNode p (outputName out)
+              tell [ arrow (outName `elem` theOutputs) oport outName ]
+
+    tell [ header [ "// Finally, we make sure the left-to-right ordering"
+                  , "// of inputs and outputs is respected."
+                  ]
+         ]
+    tell [ leftToRight "inputs" theInputs ]
+    tell [ leftToRight "outputs" theOutputs ]
+
+  theBlackbox = filter (/= "")
+    [ "subgraph cluster_" ++ show p ++ " {"
+    , "  style = invis;"
+    , indent 2 $ gateNode ++ " [label = " ++ show nm ++ "];"
+    , "  {"
+    , "    node [shape = point, height = 0];"
+    ,      unlines (map (indent 4 . declareInput p) inputs)
+        ++ unlines (map (indent 4 . declareOutput p) outputs)
+    , "  }"
+    , leftToRight "inputs" theInputs
+    , leftToRight "outputs" theOutputs
+    , connectInputs theInputs gateNode
+    , connectOutputs gateNode theOutputs
+    , "}"
+    ]
 
 def :: Def -> Dot ()
 def d = case toGate d of
   Nothing      -> pure ()
   Just (nm, g) -> do
-    id <- fresh
+    id <- freshId
     Dot $ modify $ \ s ->
       s { gates = insertArr (nm, gate nm g (gates s) . extend id) (gates s) }
 
-blackBoxDef :: Def -> [String]
-blackBoxDef d = fromMaybe [] $ do
-  st <- flip execStateT initDotSt $ runDot $ def d
+dotDef :: Def -> Maybe DotGate
+dotDef d = do
+  st <- flip execStateT initDotSt $ runDot $ do
+    -- we throw in some basic definitions so that whitebox has a change to succeed
+    -- in practice we will only need 'nand'
+    def notG
+    def andG
+    def d
   nm <- case d of { Stub{} -> Nothing; Def (nm, _) _ _ -> Just nm }
   ga <- findArr nm (gates st)
+  pure (ga empty)
+
+blackBoxDef :: Def -> [String]
+blackBoxDef d = fromMaybe [] $ do
+  ga <- dotDef d
   pure $
     [ "digraph blackbox {"
+    , "  rankdir = TB;"
+    , "  splines = ortho;"
+    ]
+    ++ blackbox ga
+    ++ ["}"]
+
+whiteBoxDef :: Def -> [String]
+whiteBoxDef d = fromMaybe [] $ do
+  ga <- dotDef d
+  pure $
+    [ "digraph whitebox {"
     , "  rankdir=TB;"
     , "  splines=ortho;"
     ]
-    ++ blackbox (ga empty)
+    ++ whitebox ga
     ++ ["}"]
+
 
 
 notG :: Def
@@ -170,8 +276,14 @@ andG = Def ("and", [PVar "x", PVar "y"]) [Var "z"] $ Just
   [ [PVar "z"] :=: [App "and" [Var "x", Var "y"]]
   ]
 
+swapG :: Def
+swapG = Def ("swap", [PVar "x", PVar "y"]) [Var "y", Var "x"] Nothing
+
 test :: IO ()
 test = do
-  let runner = putStrLn . unlines . blackBoxDef
+  let runner = putStrLn . unlines . whiteBoxDef
   runner notG
+  runner swapG
   runner andG
+  runner Anf.and4
+  runner Anf.and4'
