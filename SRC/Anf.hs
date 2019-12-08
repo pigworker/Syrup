36c0e79afc610f6989cdb0b5b0d3dca9c809a132
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-12-08 10:30:44 +0000

    [ doc ] for the Anf pass

--- SRC/Anf.hs
+++ SRC/Anf.hs
@@ -14,13 +14,25 @@ import Control.Monad.State
 import Data.Maybe
 import Syrup.SRC.Syn
 
+------------------------------------------------------------------------------
+-- Syntax of A normal forms
+
 newtype Input' nm = Input { inputName :: nm }
   deriving (Functor, Foldable, Traversable)
+
+-- Outputs may be virtual i.e. introduced by the machine but not
+-- present in the source code.
+-- For instance when we elaborate "A&B&C" we obtain "A&Z where Z = B&C".
+-- Here Z is a virtual wire and we should not display this name back
+-- to users (e.g. if we produce a circuit diagram for their source code).
 
 data Output' nm = Output
   { isVirtual  :: Bool
   , outputName :: nm
   } deriving (Functor, Foldable, Traversable)
+
+-- Expressions in A normal form: either a variable or a function applied
+-- to a bunch of variables. Nothing more complex than that.
 
 data Expr' nm
   = Alias nm
@@ -42,6 +54,9 @@ data Gate = Gate
 instance Semigroup Gate where
   a <> b = a
 
+------------------------------------------------------------------------------
+-- Fresh monad: generate fresh names for virtual wires
+
 newtype Fresh a = Fresh
   { runFresh :: State Int a }
   deriving (Functor, Applicative, Monad)
@@ -61,11 +76,18 @@ freshVirtualName = do
   i <- fresh
   pure $ '#' : show i
 
+------------------------------------------------------------------------------
+-- Elaboration
+
 elabPat :: Pat -> String
 elabPat = \case
   PVar x -> x
   PCab{} -> error "not supported yet"
 
+-- If the RHS is already a variable name then we can simply return it.
+-- Otherwise we have an arbitrary expression so we introduce a virtual
+-- name for it and return an equation connecting this virtual name to
+-- the expression.
 elabRHS :: Exp -> Fresh (Output, [Eqn])
 elabRHS = \case
   Var x -> pure (Output False x, [])
@@ -73,6 +95,9 @@ elabRHS = \case
     vn <- freshVirtualName
     pure (Output True vn, [[PVar vn] :=: [e]])
 
+-- Not much work done here: elaborate the LHS, elaborate the RHS and
+-- collect the additional equations added by doing so and finally
+-- elaborate all of the equations.
 elabDef :: Def -> Fresh (String, Gate)
 elabDef Stub{} = undefined
 elabDef (Def (nm, ps) rhs eqns) = do
@@ -87,6 +112,16 @@ elabDef (Def (nm, ps) rhs eqns) = do
        }
   pure (nm, gate)
 
+-- When elaborating an equations we have two situations:
+--    A,B,C = d,e,f
+-- or A,B,C = e
+-- The first case can be reduced to solving (A = d, B = e, C = f)
+-- The second is bit more complex:
+--   - If e is a variable then we're done.
+--   - If e is an application then we recursively elaborate all of its
+--     arguments as if they were RHS i.e. we want variable names for them
+--     and we are ready to pay for it by generating additional equations.
+--     Finally we elaborate these additional equations.
 elabEqn :: Eqn -> Fresh ([([Output], Expr)])
 elabEqn (ps :=: [rhs]) = do
   let ous = map (Output False . elabPat) ps
@@ -101,6 +136,12 @@ elabEqn (ps :=: rhs) = do
   eqns <- mapM elabEqn (zipWith (\ p e -> [p] :=: [e]) ps rhs)
   pure $ concat eqns
 
+------------------------------------------------------------------------------
+-- Back to Def
+
+-- Erase a Gate back to a Def for pretty-printing purposes.
+-- Not that we could also use this function to check that
+-- `d` and `toANF d` are bisimilar!
 fromGate :: String -> Gate -> Def
 fromGate nm g =
   Def (nm, map (PVar . inputName) (inputs g))
@@ -115,6 +156,12 @@ fromGate nm g =
                               Call f es -> App f (map (Var . inputName) es)
                            ])
                      eqns
+
+toANF :: Def -> Def
+toANF d = uncurry fromGate (evalFresh (elabDef d))
+
+------------------------------------------------------------------------------
+-- Tests
 
 foo :: Def
 foo =
@@ -136,9 +183,6 @@ and4' =
              ]
   ]
   Nothing
-
-toANF :: Def -> Def
-toANF d = uncurry fromGate (evalFresh (elabDef d))
 
 test :: IO ()
 test = do
