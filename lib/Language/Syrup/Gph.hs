f45edd9750cb583c8baf62e03d01fb5f80778bf7
Guillaume Allais <guillaume.allais@ens-lyon.org>
2026-07-03 14:57:40 +0100

    [ new ] signals thrown away

--- lib/Language/Syrup/Gph.hs
+++ lib/Language/Syrup/Gph.hs
@@ -9,14 +9,16 @@
 
 module Language.Syrup.Gph where
 
+import Data.Maybe (fromMaybe)
+
 import Language.Syrup.BigArray
 
-data Shape = Rectangle
+data Shape = Rectangle | ThrownAway
   deriving (Eq, Show)
 
 data Vertex
-  = Visible   String (Maybe Shape) -- label
-  | Invisible Bool                 -- splitting?
+  = Visible String (Maybe Shape) -- label
+  | Invisible Bool               -- splitting?
   deriving (Eq)
 
 instance Semigroup Vertex where
@@ -72,16 +74,36 @@ shrinkInvisible g@(Graph vs es) = loop g es where
             _ -> (vs, es)
       in loop (Graph vs' es') queue
 
+markDead :: Graph -> Graph
+markDead g@(Graph vs es) = loop g vs where
+
+  loop g@(Graph vs es) queue = case popArr queue of
+    Nothing -> g
+    Just ((nm, v@(Invisible False)), queue) ->
+      let vs' = case fromMaybe emptyArr $ findArr nm es of
+           arr | null arr -> insertArr (nm, Visible "" (Just ThrownAway))
+               $ deleteArr nm vs
+           _ -> vs
+      in loop (Graph vs' es) queue
+    Just (_, queue) -> loop g queue
+
 fromShape :: Maybe Shape -> String
 fromShape = \case
   Nothing        -> "none"
   Just Rectangle -> "rectangle"
+  Just ThrownAway -> "circle"
 
 fromGraph :: Graph -> ([String], [String])
 fromGraph Graph{..} =
   ( -- declare vertices first
     flip foldMapArr vertices $ \ (nm, v) -> pure $ case v of
-      Visible lb sh -> nm ++ " [shape = " ++ fromShape sh ++ ", label =\"" ++ lb ++ "\"];"
+      Visible lb sh -> concat
+        [ nm
+        , " [shape = ", fromShape sh
+        , " , label = ", show lb
+        , if sh == Just ThrownAway then ", height = 0.075" else ""
+        , "];"
+        ]
       Invisible sp  -> nm ++ " [shape = point" ++ if sp then "];" else ", height = 0];"
 
   , -- then add edges
